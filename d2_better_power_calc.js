f11301a7be749f20401f2d0eb00bcce09345aeb6
Jake Dolezal <jake.dolezal@fcsamerica.com>
2018-09-26 14:50:49 -0500

    Neatifying Code
    Commented out debug logging, use of built random function instead of Math.random for readability

--- d2_better_power_calc.js
+++ d2_better_power_calc.js
@@ -11,8 +11,8 @@ function doCalc(runs)
 				parseInt($("#classItem").val()),
 					];
 	var startAvgLight = startLight.reduce(getSum) / 8 ;
-	console.log(startLight);
-	console.log(startAvgLight);
+	//console.log(startLight);
+	//console.log(startAvgLight);
 	var avgPost = [];
 	for(j = 0; j < runs; j++) 
 	{
@@ -29,7 +29,7 @@ function doCalc(runs)
 		
 		while (totalCollected < totalLeft)
 		{
-			var piece = parseInt(Math.random() * 8);
+			var piece = random(8, 0);
 			if(avgLight < 520 && collected[0] < capEngrams[0])
 			{
 				light[piece] = dropGear(avgLight, light[piece], 8, 2);
@@ -62,10 +62,10 @@ function doCalc(runs)
 		}
 		
 		//clan engram
-		var piece = parseInt(Math.random() * 8);
+		var piece = random(8, 0);
 		light[piece] = dropGear(avgLight, light[piece], 0, 3);
 		
-		console.log(avgLight);
+		//console.log(avgLight);
 		avgPost[j] = avgLight;
 	}
 	avgPost.sort();
@@ -73,21 +73,21 @@ function doCalc(runs)
 		per50 = avgPost[avgPost.length * .5], 
 		per75 = avgPost[avgPost.length * .75];
 		
-	console.log(avgPost);
-	console.log(avgPost.length);
-	console.log(avgPost.length * .25);
-	console.log(avgPost.length * .5);
-	console.log(avgPost.length * .75);
+	//console.log(avgPost);
+	//console.log(avgPost.length);
+	//console.log(avgPost.length * .25);
+	//console.log(avgPost.length * .5);
+	//console.log(avgPost.length * .75);
 	
-	console.log(per25);
-	console.log(per50);
-	console.log(per75);
+	//console.log(per25);
+	//console.log(per50);
+	//console.log(per75);
 	
-	console.log(light);
-	console.log(startLight);
+	//console.log(light);
+	//console.log(startLight);
 		
-	console.log(avgLight);
-	console.log(startAvgLight);
+	//console.log(avgLight);
+	//console.log(startAvgLight);
 	
 	$("#val25").text(per25);
 	$("#val50").text(per50);
@@ -107,7 +107,7 @@ function random(range, lowPad)
 function dropGear(avgLight, equiped, range, lowPad)
 {
 	var newItem = random(range, lowPad) + avgLight;
-	//console.log('old: ' + equiped + ' new: ' + newItem);
+	////console.log('old: ' + equiped + ' new: ' + newItem);
 	return Math.floor((newItem > equiped) ? newItem : equiped);
 }
 
