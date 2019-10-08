413c0df1e23b3fd2807c66940e245516f5ed3f6d
Chloe Fenix <chloe.fenix3@gmail.com>
2019-10-08 14:08:44 -0500

    cb now works
    publish to dtg for feedback maybe?

--- d2_better_power_calc.js
+++ d2_better_power_calc.js
@@ -13,6 +13,12 @@ var calculator = {
 	execCalc: function(me)
 	{
 		this.attributes.runs = $("#runs").val();
+		var legacy = $(".legacyCb:checked").length;
+		var tier1 = $(".tier1Cb:checked").length;
+		var tier2 = $(".tier2Cb:checked").length;
+		var pinacle = $(".pinacleCb:checked").length;
+		this.attributes.capEngrams = [legacy, tier1, tier2, pinacle];
+		console.log(legacy);
 		this.doCalc();
 		$("#result").show();
 		$("#ran").text(this.runs);
@@ -29,10 +35,7 @@ var calculator = {
 					parseInt($("#classItem").val())
 						];
 		var artifactBonus = parseInt($("#artifact").val());
-		console.log("artifact bonus: "  + artifactBonus);
 		var startAvgLight = (startLight.reduce(this.getSum) / 8) + artifactBonus ;
-		console.log(startLight);
-		console.log(startAvgLight);
 		var avgPost = [];
 		for(j = 0; j < this.attributes.runs; j++) 
 		{
@@ -53,62 +56,38 @@ var calculator = {
 				if(collected[0] < capEngrams[0])
 				{
 					var gear = this.dropGear(avgLight, light[piece], -3, 0, 950);
-					console.log(gear)
 					light[piece] = gear;
 					collected[0]++;
 				}
 				if(collected[1] < capEngrams[1])
 				{
 					var gear = this.dropGear(avgLight, light[piece], 0, 3, 950);
-					console.log(gear)
 					light[piece] = gear;
 					collected[1]++;
 				}
 				if(collected[2] < capEngrams[2])
 				{
 					var gear = this.dropGear(avgLight, light[piece], 0, 4, 950);
-					console.log(gear)
 					light[piece] = gear;
 					collected[2]++;
 				}
 				if(collected[3] < capEngrams[3])
 				{
 					var gear = this.dropGear(avgLight, light[piece], 0, 5, 960);
-					console.log(gear)
 					light[piece] = gear;
 					collected[3]++;
 				}
 			
 			 	avgLight = (light.reduce(this.getSum) / 8) + artifactBonus;
 			 	totalCollected++;
-			 	console.log('piece: ' + piece + ' average light: ' + avgLight);
-			 	console.log(light);
 			}
 			
-			
-			console.log(avgLight);
 			avgPost[j] = avgLight;
 		}
 		avgPost.sort();
 		var per25 = avgPost[Math.floor(avgPost.length * .25)], 
 			per50 = avgPost[Math.floor(avgPost.length * .5)], 
 			per75 = avgPost[Math.floor(avgPost.length * .75)];
-			
-		console.log(avgPost);
-		console.log(avgPost.length);
-		console.log(avgPost.length * .25);
-		console.log(avgPost.length * .5);
-		console.log(avgPost.length * .75);
-		
-		console.log(per25);
-		console.log(per50);
-		console.log(per75);
-		
-		console.log(light);
-		console.log(startLight);
-			
-		console.log(avgLight);
-		console.log(startAvgLight);
 		
 		$("#primaryEnd").val(light[0]);
 		$("#energyEnd").val(light[1]);
@@ -132,7 +111,6 @@ var calculator = {
 	dropGear : function(avgLight, equiped, range, lowPad, maxLight) {
 		var newItem = this.random(range, lowPad) + avgLight;
 		newItem = (newItem > maxLight) ? maxLight : newItem;
-		//console.log('old: ' + equiped + ' new: ' + newItem);
 		return Math.floor((newItem > equiped) ? newItem : equiped);
 	},
 	copyArray : function(fromArr) {
